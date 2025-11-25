65bdb86cd87995bdb1e0a09c6ae1edbbc0e23307
Jesen Tanadi <jtanadi@gmail.com>
2025-11-25 14:59:56 -0500

    add messages

--- mastermind_ws/src/mastermind/setup.py
+++ mastermind_ws/src/mastermind/setup.py
@@ -1,30 +1,31 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
-package_name = 'mastermind'
+package_name = "mastermind"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "msg"), glob("msg/*.msg")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='root@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer="root",
+    maintainer_email="root@todo.todo",
+    description="TODO: Package description",
+    license="TODO: License declaration",
     extras_require={
-        'test': [
-            'pytest',
+        "test": [
+            "pytest",
         ],
     },
     entry_points={
-        'console_scripts': [
-            'main = mastermind.main:main'
-        ],
+        "console_scripts": ["main = mastermind.main:main"],
     },
 )
